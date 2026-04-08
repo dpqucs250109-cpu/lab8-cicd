88d362ef676df97d483b9827f8acdeed75da8b99
Ubuntu <ubuntu@ip-172-31-66-12.ec2.internal>
2026-04-08 09:12:38 +0000

    Update pipeline to deploy with Docker Compose

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const pool = new Pool({
 
 app.get('/', (req, res) => {
   res.json({
-    app:  'CISC886 Lab 8',
+    app:  'CISC 886 Lab 8',
     mode: process.env.MODE || 'local',
     node: process.version,
     host: os.hostname(),
