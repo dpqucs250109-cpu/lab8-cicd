3da0d286045b35926213dff5f6120ced6a26727d
Ubuntu <ubuntu@ip-172-31-66-12.ec2.internal>
2026-04-08 09:25:03 +0000

    Add Tea to task list

--- init.sql
+++ init.sql
@@ -9,4 +9,6 @@ INSERT INTO tasks (name, status) VALUES
   ('Eggs',         'done'),
   ('Bread',        'pending'),
   ('Butter',       'pending'),
-  ('Orange juice', 'pending');
+  ('Orange juice', 'pending'),
+  ('Tea',          'pending');
+
